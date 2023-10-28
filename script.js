6cc66f38a221a234aeed0f9add08aa4c2524f619
VIJAY-MERNDEVELOPER <vijay.spm.07@gmail.com>
2023-10-28 22:30:07 +0530

    oush

--- script.js
+++ script.js
@@ -7,19 +7,28 @@ xhr.send();
 xhr.onload = function () {
   if (xhr.status == 200) {
     const data = JSON.parse(xhr.response);
-    
+
     log(data)
+
+    log (Get all the countries from Asia continent /region using Filter function);
+    
     const countries= data.filter((value,i)=>value.region === "Asia");
       
     log(countries);
 
+    log(Get all the countries with a population of less than 2 lakhs using Filter function)
+
     const countryPopulation =data.filter((value,i)=>value.population<20000);
     log(countryPopulation);
+
+    log(Print the total population of countries using reduce function)
 
     const totalPopulation = data.reduce((acc,value)=>{
       return acc+value.population;
     },0);
     log(totalPopulation);
+
+    log(Print the country which uses US Dollars as currency.)
 
     const checkCurrency=data.map((value,i)=> {
       return (value.currencies)
@@ -27,10 +36,10 @@ xhr.onload = function () {
     log(checkCurrency);
     
     for (const name in checkCurrency) {
-      if (Object.hasOwnProperty.call(checkCurrency, name)) {
+      if (Object.hasOwnProperty.call(checkCurrency, name)=="United States dollar") {
         const element = checkCurrency[name];
         
-        log (element);
+       
       }
       
     }
