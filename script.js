06a6d9dedfde90918b507ff132ddf1daf3c60d12
VIJAY-MERNDEVELOPER <vijay.spm.07@gmail.com>
2023-10-28 22:30:36 +0530

    commit

--- script.js
+++ script.js
@@ -8,47 +8,52 @@ xhr.onload = function () {
   if (xhr.status == 200) {
     const data = JSON.parse(xhr.response);
 
-    log(data)
+    log(data);
 
-    log (Get all the countries from Asia continent /region using Filter function);
-    
-    const countries= data.filter((value,i)=>value.region === "Asia");
-      
+    log(
+      "Get all the countries from Asia continent /region using Filter function"
+    );
+
+    const countries = data.filter((value) => value.region === "Asia");
+
     log(countries);
 
-    log(Get all the countries with a population of less than 2 lakhs using Filter function)
+    log(
+      "Get all the countries with a population of less than 2 lakhs using Filter function"
+    );
 
-    const countryPopulation =data.filter((value,i)=>value.population<20000);
+    const countryPopulation = data.filter(
+      (value, i) => value.population < 20000
+    );
     log(countryPopulation);
 
-    log(Print the total population of countries using reduce function)
+    log("Print the total population of countries using reduce function");
 
-    const totalPopulation = data.reduce((acc,value)=>{
-      return acc+value.population;
-    },0);
+    const totalPopulation = data.reduce((acc, value) => {
+      return acc + value.population;
+    }, 0);
     log(totalPopulation);
 
-    log(Print the country which uses US Dollars as currency.)
+    log("Print the country which uses US Dollars as currency.");
 
-    const checkCurrency=data.map((value,i)=> {
-      return (value.currencies)
-    })
-    log(checkCurrency);
-    
-    for (const name in checkCurrency) {
-      if (Object.hasOwnProperty.call(checkCurrency, name)=="United States dollar") {
-        const element = checkCurrency[name];
-        
-       
-      }
-      
-    }
-    
+    const checkCurrency = data.filter((data) => {
+      return data.currencies && data.currencies.USD;
+    });
+    checkCurrency.forEach((value) => {
+      log(value.name.common);
+    });
 
+    log(
+      "Print the following details name, capital, flag, using forEach function"
+    );
+
+    data.forEach((value) => {
+      log(`Name : ${value.name.common}
+           Capital : ${value.capital}
+           Flag    : ${value.flags.png}
+           `);
+    });
   } else {
     console.log`request failed ${xhr.status}`;
   }
-
-
-}
-
+};
